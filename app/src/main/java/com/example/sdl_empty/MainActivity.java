eb74f19760dc53016472e3367563430dc99e8797
roomgit <2392557+roomgit@users.noreply.github.com>
2020-03-23 19:05:29 +0900

    Initial Commit

---
+++ app/src/main/java/com/example/sdl_empty/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sdl_empty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
