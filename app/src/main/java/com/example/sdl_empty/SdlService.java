9635141d42faf67e2388ae6530c6620d3cd7c9cb
roomgit <2392557+roomgit@users.noreply.github.com>
2020-03-24 17:53:32 +0900

    gradle change implementation smartdevice core

--- app/src/main/java/com/example/sdl_empty/SdlService.java
+++ app/src/main/java/com/example/sdl_empty/SdlService.java
@@ -1,13 +1,58 @@
 package com.example.sdl_empty;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
+import android.util.Log;
+
+import com.smartdevicelink.managers.SdlManager;
 
 public class SdlService extends Service {
-        //...
-        @Override
-        public IBinder onBind(Intent intent) {
-            return null;
+    private static final String APP_ID = "8678309";
+    private static final String NOTIFICATION_TITLE = "SDL App Service Running";
+    private static final int FOREGROUND_SERVICE_ID = 111;
+
+    private SdlManager sdlManager = null;
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+    @Override
+    public void onCreate() {
+        Log.d("SdlService", " onCreate");
+        super.onCreate();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            enterForeground();
         }
+    }
+    public void enterForeground() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(APP_ID, "SdlService", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(channel);
+                Notification serviceNotification = new Notification.Builder(this, channel.getId())
+                        .setContentTitle(NOTIFICATION_TITLE)
+                        .setSmallIcon(R.drawable.ic_sdl)
+                        .build();
+                startForeground(FOREGROUND_SERVICE_ID, serviceNotification);
+            }
+        }
+    }
+    public void onDestroy() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            stopForeground(true);
+        }
+
+        if (sdlManager != null) {
+            sdlManager.dispose();
+        }
+
+        super.onDestroy();
+    }
 }
